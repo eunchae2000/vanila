458f2fa9e7a650fdfe60480ef7122b79b4f14443
eunchae2000 <tldjs3651@naver.com>
2021-01-01 19:40:27 +0900

    argument, parameter, function => 함수와 변수에 대해서 배움

---
+++ vanila.js
@@ -0,0 +1,18 @@
+// function - 함수
+
+function sayHello(){
+    console.log('Hello');
+}
+
+sayHello();
+console.log('sayHello');
+
+
+// argument, parameter = 변수
+
+function Hello(name, age){
+    console.log('Hello');
+    console.log("name: ", name, "age: ", 22);
+}
+
+console.log('coco', 22);
