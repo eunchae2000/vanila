ae86443e5223c801e0f93ab6536d3a4fdece62f6
eunchae2000 <tldjs3651@naver.com>
2021-01-06 15:27:06 +0900

    document, js를 이용해서 html 내용 수정하기

--- vanila.js
+++ vanila.js
@@ -1,60 +1,22 @@
-// console은 변수보다 더 큰 object
+// js를 이용해서 html 내용을 수정하기
 
-// function의 다른 사용법
-/*function Hi(name, age){
-    console.log(`Hi ${name} you are ${age} years old`);
-}
+/* const title = document.getElementById("title");
 
-Hi("coco", 22);
+console.log(title);
+
+const title = document.getElementById("title");  // html의 title요소에 접근
+title.innerHTML = "Hi! From JS";  // html의 원래 요소를 바꾸는 속성으로 document.getElementById 메서드를 함께 사용
+title.style.color = "red";  //html의 style.color 요소의 속성을 바꾸는 것으로 사용 예시를 잘 알아야 함
+console.dir(document); // dir은 객체 => 자바스크립트 객체의 속성들을 출력
+
 */
 
-/*function Hi(name, age){
-    return `Hi ${name} you are ${age} years old`;
-}
+const title = document.querySelector("#title");  //document.querySelector는 일치하는 요소를 찾아내는 방법. 일치하는 요소가 없을 경우 null을 반환
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+console.title = "I own you now";  // 웹페이지의 제목을 바꾸는 js 방법
 
-const greet = Hi("coco", 22)
-
-console.log(greet);  // greet는 Hi의 실행된 결과 값이다.
-*/
-
-const calculator = {
-    plus: function(a, b){
-        return a + b;
-    }
-}
-
-const plus = calculator.plus(5, 5) //이 의미는 함수 기능을 객체 안에서 구현해야 됨
-console.log(plus)
-
-// 예제 문제! const, calculator를 사용해서 더하기, 나누기, 곱하기, 빼기, 제곱근을 구현하기
 /*
-const calculator = {
-plus : function(a,b){
-    return a+b
-},
-minus : function(a,b){
-    return a-b
-},
-multiple : function(a,b){
-    return a*b
-},
-divide : function(a,b){
-    return a/b
-},
-square : function(a,b){
-    return a**b
-}
-};
-
-const plus = calculator.plus(10,5);
-const minus = calculator.minus(10,5);
-const multiple = calculator.multiple(10,5);
-const divide = calculator.divide(10,5);
-const square = calculator.square(10,5);
-
-console.log(plus);
-console.log(minus);
-console.log(multiple);
-console.log(divide);
-console.log(square);
+document에 있는 모든건 다 객체가 된다
+모든 객체와 함수는 DOM(Document Object Model) 형태로 변경이 가능
 */
