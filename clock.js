c62fe1cbb8a10f5321c35163c40819e1fb50f764
eunchae2000 <tldjs3651@naver.com>
2021-01-08 11:45:51 +0900

    clock.js 파일로 시간을 실시간으로 나타내는 방법 (시간: 분: 초) 1초 단위로 나타내는 시계

---
+++ clock.js
@@ -0,0 +1,44 @@
+// 시간을 표시하는 방법
+
+/*const clockContainer = document.querySelector(".js-clock"), 
+clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}`
+}
+
+function init(){
+    getTime();
+}
+
+init();
+*/
+
+// 시간을 실시간으로 새로고침 없이 변경하는 방법
+
+const clockContainer = document.querySelector('.js-clock'), 
+ clockTitle = clockContainer.querySelector('h1');
+
+function getTime(){
+    const date = new date();
+
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:
+    ${minutes < 10 ? `0${minutes}` : minutes}:
+    ${seconds < 10 ? `0${seconds}`: seconds}`;
+
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
