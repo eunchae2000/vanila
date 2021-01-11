738fa256679b6ff37b5bbffbfe30ff375477b5ce
eunchae2000 <tldjs3651@naver.com>
2021-01-12 01:20:26 +0900

    todolist에 쓰이는 함수와 구조에 대해서 공부

--- todo.js
+++ todo.js
@@ -5,7 +5,9 @@ const toDoList = document.querySelector(".js-toDoList");
  const TODO_LS = "toDo";
 
  let toDo = [];
+ // 함수 내에서 재할당해야 되기 때문에 let을 사용
 
+ // todolist 항목을 선택적으로 삭제하는 기능
  function deleteToDo(event){
      const btn = event.target;  
      const li = btn. parentNode;  
@@ -16,38 +18,59 @@ const toDoList = document.querySelector(".js-toDoList");
      toDo = cleanToDo;
      saveToDo();
 
-     // event.target으로 이벤트 객체를 받아옴
-     // parentNode 노드는 부모노드로 선언하고 빈 공간을 포함
-     // removeChild 는 문서 계층 구조(toDoList)에서 하나의 노드 li를 삭제하는 것
-     // toDo.filter(function(toDo) 는 toDo라는 배열을 순회하며 요소마다 조건 
-     // 확인 후 조건을 만족하는 원소들로 구성된 새로운 배열 리턴
-     // toDo id는 숫자, li id는 string이다. 그래서 parseInt 함수를 이용해 문자열을 정수로 바꿔준다.
-     // toDo를 새로운 배열로 만든 cleanToDo로 다시 정함
-     // saveToDo로 todolist를 저장
+     /*
+      + event가 어떤 버튼에서 발생했는지 알기 위해서 event.target으로 사용
+      + 해당하는 li도 삭제하기 위해서 btn에 부모노드를 추가
+      + removeChild 는 노드 li의 자식노드를 삭제하는 것
+      + toDo.filter(function(toDo) 는 toDo라는 배열을 순회하며 요소마다 조건 
+      + 확인 후 조건을 만족하는 원소들로 구성된 새로운 배열 리턴
+      + toDo id는 숫자, li id는 string이다. 그래서 parseInt 함수를 이용해 문자열을 정수로 바꿔줌
+      + toDo를 새로운 배열로 만든 cleanToDo로 다시 정함
+      + saveToDo로 todolist를 localStorage에 저장
+     */
  }
 
+ // 작성한 todolist를 string 형태로 localStorage에 저장
  function saveToDo(){
      localStorage.setItem(TODO_LS, JSON.stringify(toDo));
 
-     // localStorage.setItem은 TODO_LS에 JSON.stringify(toDO) 데이터를 쓰는 것으로
-     // localStorage에는 javaScript의 data를 저장 할 수 없고, 오직 strig 형태만 저장 가능
-     // JSON.stringify(toDo)는 한번에 한 객체를 통체로 저장하는 방법으로 한번의 todolist가 작성될 때 마다 저장
+     /* 
+      + localStorage.setItem은 TODO_LS에 JSON.stringify(toDO) 객체로 이루어진 데이터를 string 형태로 쓰는 것,
+      localStorage에는 javaScript의 data를 저장 할 수 없고, 오직 strig 형태만 저장 가능
+      + JSON.stringify(toDo)는 한번에 한 객체를 통체로 저장하는 방법으로 한번의 todolist가 작성될 때 마다 저장
+     */
 
  }
 
+ //
  function paintToDo(text){
-     const li = document.createElement("li");   // createElement()는 요소를 만드는 것으로 html의 li 요소를 만드는 것
-     const delBtn = document.createElement("button");   //
+     const li = document.createElement("li");   
+     const delBtn = document.createElement("button");
      const span = document.createElement("span");
      const newId = toDo.length + 1;
 
-     delBtn.innerText = "♬";
+     /*
+      + createElement()는 요소를 만드는 것으로 html의 li 요소를 만드는 것
+      + toDo 배열의 길이를 추가
+     */
+
+     delBtn.innerText = "❌";
      delBtn.addEventListener("click", deleteToDo);
      span.innerText = text;
+
+     /*
+      + innerText는 "❌"의 값만 가져오는 것
+      + click이라는 이벤트 효과로 "❌"를 클릭했을 때 선택한 todolist 항목이 삭제
+      */
 
      li.appendChild(span);
      li.appendChild(delBtn);
      li.id = newId;
+
+     /*
+      + appendChild는 li에 span과 delBtn을 자식노드로 추가
+      + newId는 li와 toDo에 id를 설정해 각각의 id를 설정해 주기 위함
+      */
 
      toDoList.appendChild(li);
      const toDoObj = {
@@ -56,15 +79,31 @@ const toDoList = document.querySelector(".js-toDoList");
      };
      toDo.push(toDoObj);
      saveToDo();
+
+     /* 
+      + li항목을 toDoList 항목에 자식노드로 추가
+      + 새로 입력된 값을 잠시 담는 변수이고 push를 이용해서 기존의 toDo에 입력
+      + saveToDo 함수를 이용해 새롭게 입력된 값을 저장
+     */
  }
 
+ // 기존 기능을 없애고 todolist 안에 데이터를 입력하여 값을 저장
+ // submit event를 처리하는 함수
  function handleSubmit(event){
      event.preventDefault();
      const currentValue = toDoInput.value;
      paintToDo(currentValue);
      toDoInput.value = "";
+
+     /*
+      + preventDefault() 기본동작인 enter를 누를 때마다 새로고침이 되는 고유의 동작을 중단시키는 역할
+      + toDoInput에 새로운 값을 넣고 그 값을 todolist 항목에 넣어주는 것
+      + paintToDo() 함수의 인자로 currentValue에 담고 실행 => todolist 추가
+      + toDoInput의 input 영역을 공백으로 지정해주기 위해서 ""로 지정
+     */
  }
 
+ // 저장된 todolist를 불러오는 것
  function loadTodo(){
      const loadTodo = localStorage.getItem(TODO_LS);
      if(loadTodo !== null){
@@ -73,11 +112,21 @@ const toDoList = document.querySelector(".js-toDoList");
              paintToDo(toDo.text);
          });
      }
+
+     /*
+     + localStorage에 TODO_LS로 저장된 값을 가져오는 것
+     + JSON.parse()는 string 객체를 json 객체로 변환 시켜주는 것
+     + forEach()는 주어진 함수를 배열 요소 각각에 대해 실행, 배열 객체에서만 사용 가능
+     */
+
  }
 
  function init(){
      loadTodo();
      toDoForm.addEventListener("submit", handleSubmit);
+
+     // loadTodo()를 실행하고 사용자의 상황에 따라 다음 과정을 순차적으로 진행
+     // "submit"이라는 전송하는 이벤트를 실행하면 handleSubmit() 내용을 실행시켜 todolist의 기능을 빠짐없이 수행하도록 한다.
  }
 
  init();
--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const weather = document.querySelector(".js-weather");
-const API_KEY = "1684e7a72f33d056e3bccb08769f6063";
+const API_KEY = "1684e7a72f33d056e3bccb08769f6063";     // open weather 로 API 주소를 받아옴
 const COORDS = 'coords';
 
 function getWeather(lat, lng){
@@ -13,29 +13,50 @@ function getWeather(lat, lng){
             const place = json.name;
             weather.innerText = `${temperature}ºc / ${place}`;
         });
+        
+        //fetch() 함수는 첫 번째 인자로 url, 두 번째 인자로 옵션 객체를 받는다
+        // 반환된 객체가 API 호출이 성공했을 경우 응답(response) 객체를 반환
+        // response에서 JSON Data로 가져온 것을 보류하지 않고 바로 JSON Data를 출력 (F12를 통해서 가져온 JSON Data)
+
+        // temperature, place를 각각 JSON Data로 설정
+        // weather.innerText에서 `~` 사이에 $으로 각각 인자화해서 html에 출력
 }
 
+// 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+    
+    // COORDS에 있는 coordsObj의 값을 string 값으로 localStorage에 가져온다
 }
 
+// 정보를 가져오는 것에 대해 성공했을 경우
 function handleGeoSucces(position){
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+    const latitude = position.coords.latitude;     // 위도
+    const longitude = position.coords.longitude;   // 경도
     const coords = {
         latitude: latitude,
         longitude: longitude
     };
     saveCoords(coords);
     getWeather(latitude, longitude);
+
+    // position.coords.latitude 현재 위치의 위도
+    // position.coords.longitude 현재 위치의 경도
+    // 위도와 경도의 정보를 저장
+    // 현재 위도와 경도를 기준으로 날씨 정보를 가져옴
 }
 
+// 정보를 가져오는 것에 대해 실패했을 경우
 function handleGeoError(){
     console.log("Can't access geo location");
 }
 
+
+// 위치와 경도의 정보가 없을 때 가져오는 것
 function askForCoords(){
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+    // geolocation.getCurrentPosition()는 장치의 현재 위치를 가져오는 것
+    // navigator 객체는 브라우저에 대한 정보를 가지고 있는 객체
 }
 
 function loadCords(){
@@ -46,6 +67,11 @@ function loadCords(){
         const parseCoords = JSON.parse(loadCords);
         getWeather(parseCoords.latitude, parseCoords.longitude);
     }
+
+    // loadCords()를 호출하여 localStorage에 정보를 가져옴
+    // 만약 loadCords의 정보가 없다면 askForcoords를 호출하여 위도와 경도를 가져옴
+    // loadCords의 정보가 있다면 parseCoords의 정보를 JSON.parse를 이용해서 문자열을 객체로 변환하여
+    // 날씨 정보를 가져옴(getWeather)
 }
 
 function init(){
